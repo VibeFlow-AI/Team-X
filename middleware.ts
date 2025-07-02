70394cd884b6352936e78f9a58bb3abc2200e49c
sahan-chinthaka <sahanchinthaka04@gmail.com>
2025-07-02 15:53:44 +0530

    feat: enhance middleware with role-based route protection

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,33 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher(["/get-started(.*)"]);
+
+const isMentorRoute = createRouteMatcher(["/mentor/dashboard(.*)"]);
+
+const isStudentRoute = createRouteMatcher(["/student/dashboard(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    await auth.protect();
+  }
+
+  if (isMentorRoute(req)) {
+    const { sessionClaims } = await auth.protect();
+
+    if (sessionClaims?.metadata?.role !== "mentor") {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  }
+
+  if (isStudentRoute(req)) {
+    const { sessionClaims } = await auth.protect();
+
+    if (sessionClaims?.metadata?.role !== "student") {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  }
+});
 
 export const config = {
   matcher: [
