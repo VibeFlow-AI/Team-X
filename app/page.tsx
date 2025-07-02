71e77ce8fe282e80794d64278a24e0dece125386
Dulran Hemjitha <dhemjitha@gmail.com>
2025-07-02 12:20:37 +0530

    feat: Update configuration and styles; add new dependencies and course section component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { CoursesSection } from "@/components/courses-section";
 import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <CoursesSection />
     </main>
   );
 }
---
+++ types/index.ts
@@ -0,0 +1,16 @@
+export interface Course {
+    initials: string
+    title: string
+    author: string
+    tags: string[]
+    description: string
+    features: string[]
+    duration: string
+    idealFor: string
+  }
+  
+  export interface Feature {
+    title: string
+    img: string
+  }
+  
